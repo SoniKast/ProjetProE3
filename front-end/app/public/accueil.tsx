15d30071dd65fd045604fddf963263d8700e81be
balla <52130402+SoniKast@users.noreply.github.com>
2025-05-23 15:43:01 +0200

    carousel

--- front-end/app/public/accueil.tsx
+++ front-end/app/public/accueil.tsx
@@ -3,18 +3,32 @@ import { Link } from "react-router";
 export function Accueil() {
     return (
         <>
-            <section className="py-5 text-center bg-primary">
-                <div className="row py-lg-5">
-                    <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light text-light">Album example</h1>
-                        <p className="lead text-light">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                        <p>
-                        <Link to="#" className="btn btn-success my-2">Main call to action</Link>
-                        <Link to="#" className="btn btn-secondary my-2">Secondary action</Link>
-                        </p>
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img src="../assets/6bdb69f2e082e2074015098f4a14f6b0.png" className="d-block w-100" />
+                    </div>
+                    <div className="carousel-item">
+                    <img src="../assets/94d3b3a61c62a69250d8475788036df9--shadow-the-hedgehog-brave.jpg" className="d-block w-100" />
+                    </div>
+                    <div className="carousel-item">
+                    <img src="../assets/599fbc6b956a9c27dae863e0da4a611929387764_s2_n1.png" className="d-block w-100" />
                     </div>
                 </div>
-            </section>
+                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
         </>
     );
 }
